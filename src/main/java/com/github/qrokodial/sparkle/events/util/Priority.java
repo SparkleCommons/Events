9abdda630c04d49a399ea9f9bd141ef1c01b166d
Michael Zanca <qrok@projectbnet.net>
2015-09-07 16:50:36 -0400

    Allow for getting the inverse of a priority

--- src/main/java/com/github/qrokodial/sparkle/events/util/Priority.java
+++ src/main/java/com/github/qrokodial/sparkle/events/util/Priority.java
@@ -4,35 +4,57 @@ public enum Priority {
     /**
      * For core functionality. Do not use unless you know exactly what you're doing. If unsure, use {@link #HIGHEST}.
      */
-    REALTIME,
+    REALTIME(3),
 
     /**
      * For things requiring the highest priority.
      */
-    HIGHEST,
+    HIGHEST(2),
 
     /**
      * For things requiring higher priority.
      */
-    HIGH,
+    HIGH(1),
 
     /**
      * The default priority. If unsure, use this.
      */
-    NORMAL,
+    NORMAL(0),
 
     /**
      * For things of low priority.
      */
-    LOW,
+    LOW(-1),
 
     /**
      * For things of the lowest, non-monitoring priority. If you need to modify data, use this; Otherwise use {@link #MONITOR}.
      */
-    LOWEST,
+    LOWEST(-2),
 
     /**
      * For things that need to monitor, but not modify.
      */
-    MONITOR,
+    MONITOR(-3);
+
+    private int id;
+
+    Priority(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the inverse of the priority. {@link #HIGHEST} becomes {@link #LOWEST}, {@link #LOWEST} becomes
+     * {@link #HIGHEST}, and so on.
+     *
+     * @return the inverse of the priority
+     */
+    public Priority inverse() {
+        int id = -this.id;
+        for (Priority priority : Priority.values()) {
+            if (priority.id == id) {
+                return priority;
+            }
+        }
+        return null;
+    }
 }
